f362b4634692f883cffc764de699d0fc88bf2f1e
Cali Tabilas <ctabilas23@gmail.com>
2019-08-01 12:26:02 -0400

    skeleton for other requests added

--- server/controllers.js
+++ server/controllers.js
@@ -12,50 +12,110 @@ const pool = new Pool({
 // const queries = require('./queries');
 
 const dummyController = (req, res) => {
-  // res.json({ info: 'Node.js, Express, and Postgres API' });
-  // res.send(`fixed start script`);
-  pool.query(
-    // 'SELECT * FROM questions WHERE question_id < 10',
-    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ',
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      res.status(200).send(results.rows);
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    res.status(200).send(results.rows);
+  });
 };
 
 const getQuestions = (res, req) => {
-  res.send(`get questiosn working`);
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const getAnswers = (res, req) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const postQuestion = (res, req) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const postAnswer = (req, res) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const helpfulQuestion = (req, res) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const reportQuestion = (req, res) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const helpfulAnswer = (req, res) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 const reportAnswer = (req, res) => {
-  //
+  // TODO: fix this query
+  const queryEntry =
+    'SELECT * FROM questions WHERE question_id < 10 AND product_id = 1 ORDER BY question_date DESC ';
+  pool.query(queryEntry, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 };
 
 module.exports = {
