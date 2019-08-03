a32fd30ca8b8ef0a0fe4c99223948cb9a8ad4242
Cali Tabilas <ctabilas23@gmail.com>
2019-08-03 08:14:33 -0400

    edits on db filespath

--- seedData.js
+++ seedData.js
@@ -1,46 +1,13 @@
-// const fs = require('fs');
-// let csv = require('fast-csv');
-// const path = require('path');
-
-// const db = require('./server/database.js');
-
-// let count = 0;
-
-// let options = { highWaterMark: 256 * 1024 };
-
-// fs.createReadStream(
-//   path.join(__dirname, '../apiCSVs/answers_photos.csv'),
-//   options
-// )
-//   .pipe(csv.parse({ headers: true }))
-//   .on('data', (row) => {
-//     let photo = JSON.stringify({ id: row.id, url: row['url'] });
-//     const queryEntry = `UPDATE answers SET photos = photos || $2::jsonb WHERE answer_id = $1;`;
-//     const photosArr = [row[('answer_id', photo)]];
-//     db.query(queryEntry, photosArr, (err, results) => {
-//       if (err) {
-//         count++;
-//         if ((count % 100, 000 === 0)) {
-//           console.log(count);
-//         }
-//       }
-//     });
-//   })
-//   .on('end', () => {
-//     console.log(`closed now`);
-//   })
-//   .on('err', (err) => {
-//     console.log(`seeding err`, err);
-//   });
-
-var csv = require('fast-csv');
-const path = require('path');
-const db = require('./database/database.js');
 const fs = require('fs');
+const csv = require('fast-csv');
+const path = require('path');
+
+const db = require('./server/database.js');
 
 let count = 0;
 
 let options = { highWaterMark: 256 * 1024 };
+
 fs.createReadStream(
   path.join(__dirname, '../apiCSVs/answers_photos.csv'),
   options
@@ -50,6 +17,7 @@ fs.createReadStream(
     let photo = JSON.stringify({ id: row.id, url: row[' url'] });
     let queryEntry = `UPDATE answers SET photos = photos || $2::jsonb WHERE answer_id = $1;`;
     let arrEntry = [row[' answer_id'], photo];
+
     db.query(queryEntry, arrEntry, (err, result) => {
       if (err) console.log(err);
       count++;
