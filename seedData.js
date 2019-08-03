855db713ddaed6d5464a051f4ad081fec94bfec3
Cali Tabilas <ctabilas23@gmail.com>
2019-08-03 07:12:12 -0400

    seed data function written

---
+++ seedData.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const csv = require('fast-csv');
+const path = require('path');
+
+const db = require('./server/database.js');
+
+let count = 0;
+
+let options = { highWaterMark: 256 * 1024 };
+
+fs.createReadStream(
+  path.join(__dirname, '../apiCSVs/answers_photos.csv'),
+  options
+)
+  .pipe(csv.parse({ headers: true }))
+  .on('data', (row) => {
+    let photo = JSON.stringify({ id: row.id, url: row['url'] });
+    const queryEntry = `UPDATE answers SET photos = photos || $2::jsonb WHERE answer_id = $1;`;
+    const photosArr = [row[('answer_id', photo)]];
+    db.query(queryEntry, photosArr, (err, results) => {
+      if (err) {
+        count++;
+        if ((count % 100, 000 === 0)) {
+          console.log(count);
+        }
+      }
+    });
+  })
+  .on('end', () => {
+    console.log(`done adding photos`);
+  })
+  .on('err', (err) => {
+    console.log(`seeding err`, err);
+  });
