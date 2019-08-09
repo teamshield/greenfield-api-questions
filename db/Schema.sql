0e522ecc71164566eccecb3ad992ff611f33e957
Cali Tabilas <ctabilas23@gmail.com>
2019-08-09 14:31:18 -0400

    edits on schema so no reseed

--- db/Schema.sql
+++ db/Schema.sql
@@ -1,55 +1,57 @@
-DROP TABLE IF EXISTS questions;
-CREATE TABLE questions
-(
-  question_id INT NOT NULL,
-  product_id INT NOT NULL,
-  question_body TEXT NOT NULL,
-  question_date date,
-  asker_name TEXT NOT NULL,
-  asker_email TEXT NOT NULL,
-  reported INT,
-  question_helpfulness INT NOT NULL,
-  CONSTRAINT "Questions_pkey" PRIMARY KEY
-(question_id)
-)
-WITH
-(
-    OIDS = FALSE
-);
+-- DROP TABLE IF EXISTS questions;
+-- CREATE TABLE questions
+-- (
+--   question_id INT NOT NULL,
+--   product_id INT NOT NULL,
+--   question_body TEXT NOT NULL,
+--   question_date date,
+--   asker_name TEXT NOT NULL,
+--   asker_email TEXT NOT NULL,
+--   reported INT,
+--   question_helpfulness INT NOT NULL,
+--   CONSTRAINT "Questions_pkey" PRIMARY KEY
+-- (question_id)
+-- )
+-- WITH
+-- (
+--     OIDS = FALSE
+-- );
 
 
 
-DROP TABLE IF EXISTS new_answers;
-CREATE TABLE new_answers
-(
-  answer_id INT NOT NULL,
-  question_id INT,
-  body TEXT,
-  date date,
-  answerer_name TEXT,
-  answerer_email TEXT,
-  report INT,
-  helpfulness INT,
-  photos JSON
-  [],
-  CONSTRAINT "New_Answers_pkey" PRIMARY KEY
-  (answer_id)
-)
-  WITH
-  (
-  OIDS = FALSE
-);
+-- DROP TABLE IF EXISTS new_answers;
+-- CREATE TABLE new_answers
+-- (
+--   answer_id INT NOT NULL,
+--   question_id INT,
+--   body TEXT,
+--   date date,
+--   answerer_name TEXT,
+--   answerer_email TEXT,
+--   report INT,
+--   helpfulness INT,
+--   photos JSON
+--   [],
+--   CONSTRAINT "New_Answers_pkey" PRIMARY KEY
+--   (answer_id)
+-- )
+--   WITH
+--   (
+--   OIDS = FALSE
+-- );
 
--- SEED FUNCTIONS
-\COPY questions FROM '/docker-entrypoint-initdb.d/csv/questions.csv' DELIMITERS ',' CSV header;
+-- -- SEED FUNCTIONS
+-- \COPY questions FROM '/docker-entrypoint-initdb.d/csv/questions.csv' DELIMITERS ',' CSV header;
 
-  \COPY new_answers FROM '/docker-entrypoint-initdb.d/csv/new_answers.csv' DELIMITERS ',' CSV header;
+--   \COPY new_answers FROM '/docker-entrypoint-initdb.d/csv/new_answers.csv' DELIMITERS ',' CSV header;
 
-  -- INDEXING
+-- INDEXING
 
-  CREATE INDEX ON questions
-  (product_id);
-  -- CREATE INDEX ON questions(product_id) WHERE reported = 0;
-  CREATE INDEX ON new_answers
-  (question_id);
-  -- CREATE INDEX ON new_answers(question_id) WHERE report = 0
+CREATE INDEX ON questions
+(product_id);
+CREATE INDEX ON questions
+(product_id) WHERE reported = 0;
+CREATE INDEX ON new_answers
+(question_id);
+CREATE INDEX ON new_answers
+(question_id) WHERE report = 0
