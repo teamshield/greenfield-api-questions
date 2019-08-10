7abfbbdb9b04b0c7b4a77d1c754e46c4dd704a1a
Cali Tabilas <ctabilas23@gmail.com>
2019-08-10 10:47:03 -0400

    added a console log to see psql config

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 const { PSQL_CONFIG } = require('../config.js');
 
 // Testing to see which config is passed
-// console.log('\n PSQL_CONFIG - in database.js \n', PSQL_CONFIG);
+console.log('\n PSQL_CONFIG - in database.js \n', PSQL_CONFIG);
 
 const db = pgp(PSQL_CONFIG);
 
