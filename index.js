bac96a005dfaec170d412e41dd24d300d7e74073
Cali Tabilas <ctabilas23@gmail.com>
2019-08-10 12:06:18 -0400

    added .txt to sendfile

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use('/', router);
 
 // FIXME: fix this endpoint for testing
 app.get(`/loaderio-031062197befdc8047c246214c7f9648`, (req, res) => {
-  res.sendFile('./loader/031062197befdc8047c246214c7f9648.txt');
+  res.sendFile(
+    '/home/ec2-user/greenfield-api-questions/loader/031062197befdc8047c246214c7f9648.txt'
+  );
 });
 
 const PORT = process.env.PORT || 4000;
